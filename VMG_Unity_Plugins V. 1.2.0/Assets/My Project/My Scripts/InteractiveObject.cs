132f4ad877397f74a7a1148a87b3214a21b2e804
David Tu <david.tu2@csu.fullerton.edu>
2019-01-25 17:07:09 -0800

    Added Door Controller

--- VMG_Unity_Plugins V. 1.2.0/Assets/My Project/My Scripts/InteractiveObject.cs
+++ VMG_Unity_Plugins V. 1.2.0/Assets/My Project/My Scripts/InteractiveObject.cs
@@ -5,16 +5,57 @@ using UnityEngine;
 //Treat this as a COMPONENT of the attached object (i.e. like a new instance of a class)
 //This script is attached to Door Pivot
 public class InteractiveObject : MonoBehaviour {
-
-    //My own method
-    public void triggerInteraction(){
-        Debug.Log("Trigger Interaction has occured");
+    public enum state{
+        active, //Open
+        inactive //Close
     }
 
-    //Seems to only work if I set isTrigger = True
-    private void OnTriggerEnter(Collider other){
-        if (other.name == "Sphere"){
+    public GameObject interactiveObject;
+    Animator controller;
+    private state current;
+
+    private void Start(){
+        current = state.inactive;
+        interactiveObject = GameObject.FindGameObjectWithTag("DoorPivot");
+        controller = interactiveObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other){ //Seems to only work if I set isTrigger = True
+        if (other.gameObject.tag == "Hand"){
+            //triggerInteractionLegacy(); //To use this, enable Animation and disable the Animator
             triggerInteraction();
         }
+    }
+
+    public void triggerInteraction(){
+        if (controller.GetCurrentAnimatorStateInfo(0).IsName("DoorIdle")){ //"0" refers to the layer in the Animator. In this case, it is the Base Layer
+            doors("Open");
+        } else if (controller.GetCurrentAnimatorStateInfo(0).IsName("DoorOpen")){
+            doors("Close");
+        }
+    }
+
+    public void triggerInteractionLegacy(){
+        if (!interactiveObject.GetComponent<Animation>().isPlaying){ //This check is necessary to prevent the object from jittering
+                switch (current){
+                case state.active:
+                    interactiveObject.GetComponent<Animation>().Play("Close");
+                    current = state.inactive;
+                    break;
+
+                case state.inactive:
+                    interactiveObject.GetComponent<Animation>().Play("Open");
+                    current = state.active;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    void doors(string current){
+        Debug.Log(controller);
+        controller.SetTrigger(current);
     }
 }
