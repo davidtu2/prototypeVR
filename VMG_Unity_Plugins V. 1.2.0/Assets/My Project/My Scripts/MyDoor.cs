3441ba5000890f30bcbf76a411256c1e0aad4af6
David Tu <david.tu2@csu.fullerton.edu>
2019-03-13 10:05:36 -0700

    Update MyDoor.cs

--- VMG_Unity_Plugins V. 1.2.0/Assets/My Project/My Scripts/MyDoor.cs
+++ VMG_Unity_Plugins V. 1.2.0/Assets/My Project/My Scripts/MyDoor.cs
@@ -3,52 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MyDoor : MonoBehaviour {
-    //Animators
-	private Animator animatorDoor;
-    private GameObject ARMS; //Game object for the ARMS. Need it to access it's animator
-    private Animator animatorARMS;
+    //GameObject(s)
+    private GameObject ARMS; //Needed to access it's animator component
 
-    //Determines which direction the player is facing the door
-    //For instance, if the player is exiting, he is facing the BACK of the door
-    bool exiting;
+    //Animators
+    private Animator animatorDoor;
+    private Animator animatorARMS;
 
     //Initialization
     void Start (){
-		animatorDoor = transform.Find("Door_01").GetComponent<Animator>(); //Get the TRANSFORM's name from the tree
         ARMS = GameObject.FindGameObjectWithTag("ArmsObject1");
+
+        animatorDoor = transform.Find("Door_01").GetComponent<Animator>(); //Get the Transform with the specified name from the object hierarchy
         animatorARMS = ARMS.GetComponent<Animator>();
-        exiting = false;
     }
 
-    //Will be enabled if isTrigger = true
-    void OnTriggerEnter (Collider other){
-        //Case: The player is facing the front of the door
+    void OnTriggerEnter (Collider other){ //Will be enabled if isTrigger = true
+        //Case 1: The player is facing the front of the door
         //"Empty" means that the door is idle and "0" refers to the layer in the Animator. In this case, it is the Base Layer
         if (other.gameObject.tag == "Hand" &&
             animatorDoor.GetCurrentAnimatorStateInfo(0).IsName("Empty") && animatorARMS.GetCurrentAnimatorStateInfo(0).IsName("Attack")){
             //door("DoorOpen");
-            OpenDoor();
-            exiting = true;
+            switchDoorState();
 
             //Once the door opens, immediately stop the punching animation
-            if (animatorARMS.GetCurrentAnimatorStateInfo(0).IsName("Attack")){
+            if (animatorARMS.GetCurrentAnimatorStateInfo(0).IsName("Attack")){//TODO: Also check if the door is opened?
                 protag("Idle");
             }
         }
 
-        //Case: The player is facing the back of the door and is waiting for it to re-open
-        else if (other.gameObject.tag == "Body" && animatorDoor.GetCurrentAnimatorStateInfo(0).IsName("Empty") && exiting){
+        //Case 2: The player is facing the back of the door and is waiting for it to re-open
+        else if (other.gameObject.tag == "Body" && animatorDoor.GetCurrentAnimatorStateInfo(0).IsName("Empty") && 
+            behindDoor(other.transform.position)){
             //door("DoorOpen");
-            OpenDoor();
-            exiting = false;
+            switchDoorState();
         }
 	}
 
 	void OnTriggerExit (Collider other){
-        if (other.gameObject.tag == "Body" &&
-            animatorDoor.GetCurrentAnimatorStateInfo(0).IsName("OpenDoor")){
+        if (other.gameObject.tag == "Body" && animatorDoor.GetCurrentAnimatorStateInfo(0).IsName("OpenDoor")){
             //door("DoorClose");
-            OpenDoor();
+            switchDoorState();
         }
     }
 
@@ -75,19 +70,36 @@ public class MyDoor : MonoBehaviour {
     }
 
     void protag(string state){
+        //Check if the player is already in the specified state, otherwise, switch to it
         if (!animatorARMS.GetCurrentAnimatorStateInfo(0).IsName(state)){
             animatorARMS.SetTrigger(state);
         }
     }
 
     /*void door(string state){
-        animatorDoor.SetTrigger(state);
+        //Check if the door is already in the specified state, otherwise, switch to it
+        if (!animatorDoor.GetCurrentAnimatorStateInfo(0).IsName(state)){
+            animatorDoor.SetTrigger(state);
+        }
     }*/
 
-    void OpenDoor(){
+    void switchDoorState(){
         if (animatorDoor != null){
             //Will switch between true and false
             animatorDoor.SetBool("IsOpen", !animatorDoor.GetBool("IsOpen"));
+        }
+    }
+
+    bool behindDoor(Vector3 position){
+        Vector3 toPlayer = (position - transform.position).normalized;
+
+        Debug.Log(Vector3.Dot(toPlayer, transform.forward));
+
+        //If the player is behind the door, the dot product should return any value between the range (0, -1]
+        if (Vector3.Dot(toPlayer, transform.forward) < 0){
+            return true;
+        } else{
+            return false;
         }
     }
 }
