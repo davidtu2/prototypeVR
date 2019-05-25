b4015454e94bda93ca6ab20fa8a025019e7af80a
David Tu <david.tu2@csu.fullerton.edu>
2019-05-25 13:21:16 -0700

    Update MyDoor.cs

--- VMG_Unity_Plugins V. 1.2.0/Assets/My Project/My Scripts/MyDoor.cs
+++ VMG_Unity_Plugins V. 1.2.0/Assets/My Project/My Scripts/MyDoor.cs
@@ -8,46 +8,15 @@ public class MyDoor : MonoBehaviour {
     public bool locked;
     private UI UI;
 
-    private GameObject subDoor;
-
     private void Start (){
         //This gets the Transform with the specified name from the object hierarchy
         animatorDoor = transform.Find("Door_01").GetComponent<Animator>();
         UI = GameObject.FindGameObjectWithTag("InteractionUI").GetComponent<UI>();
         locked = true;
-
         nametag = gameObject.tag;
-        subDoor = transform.Find("Door_01").gameObject;
     }
 
-    //This control struc be enabled if isTrigger = true
-    private void OnTriggerEnter (Collider other){
-        //"Empty" means that the door is idle and "0" refers to the layer in the Animator. In this case, it is the Base Layer
-        /*if (other.gameObject.tag == "Body" && animatorDoor.GetCurrentAnimatorStateInfo(0).IsName("Empty")) {
-            if (!locked) {
-                if (!isBehindDoor(other.transform.position)) {
-                    if (UI.getCanvas() != null){
-                        UI.setDoorStateText("Punch the door to open");
-                        UI.setPanel("Door", true);
-                    }
-                } else {
-                    //The player is facing the back of the door and is waiting for it to re-open
-                    toggleDoorState();
-                }
-            } else {
-                if (UI.getCanvas() != null){
-                    UI.setDoorStateText("The door is locked");
-                    UI.setPanel("Door", true);
-                }
-            }
-        }*/
-	}
-
 	private void OnTriggerExit (Collider other){
-        if (UI.getCanvas() != null){
-            UI.setPanel("Door", false);
-        }
-
         if (other.gameObject.tag == "Body" && animatorDoor.GetCurrentAnimatorStateInfo(0).IsName("OpenDoor")){
             toggleDoorState();
         }
@@ -55,68 +24,66 @@ public class MyDoor : MonoBehaviour {
 
     //Used to check if an object is already inside the trigger zone
     private void OnTriggerStay(Collider other){
-        if (other.gameObject.tag == "Hand"){ //Hand will generate upon punching
+        //CASE: Hand is detected within the trigger for some time
+        if (other.gameObject.tag == "Hand"){
+            //"Empty" means that the door is idle and "0" refers to the layer in the Animator. In this case, it is the Base Layer
             if (animatorDoor.GetCurrentAnimatorStateInfo(0).IsName("Empty") && !locked && !isBehindDoor(other.transform.position) && !animatorDoor.GetBool("IsOpen")){
                 toggleDoorState();
             }
         }
 
-        //CASE: If the player is within the trigger zone and the door is closed
+        //CASE: If the player is within the trigger and the door is closed
         if (other.gameObject.tag == "Body" && !animatorDoor.GetBool("IsOpen")){
             if (!locked){
-                if (isFacingDoor(other.transform)){
-                    if (!isBehindDoor(other.transform.position)){
+                //SUB-CASE: if the player is in front of an unlocked door
+                if (!isBehindDoor(other.transform.position)) {
+                    Ray ray = new Ray(other.transform.position, other.transform.forward);
+                    if (isFacingDoor(ray)) {
                         UI.setDoorStateText("Punch the door to open");
                         UI.setPanel("Door", true);
                     } else {
-                        //The player is facing the back of the door and is waiting for it to re-open
-                        toggleDoorState();
+                        UI.setPanel("Door", false);
                     }
+
+                //SUB-CASE: if the player is behind an unlocked door. I doesn't matter if the player is facing it, the door will open
                 } else {
-                    UI.setPanel("Door", false);
+                    toggleDoorState();
                 }
+
             } else {
+                //SUB-CASE: When the player is in front of a locked door
                 if (!isBehindDoor(other.transform.position)){
-                    if (isFacingDoor(other.transform)){
+                    Ray ray = new Ray(other.transform.position, other.transform.forward);
+                    if (isFacingDoor(ray)){
                         UI.setDoorStateText("The door is locked");
                         UI.setPanel("Door", true);
                     } else {
                         UI.setPanel("Door", false);
                     }
-                } else {//TODO: Buggy: If player is behind a locked door and NOT facing it, the notification should disappear. Works with OnTriggerExit() to turn off the notification. Perhaps put a collider within the collider?
-                    //UI.setDoorStateText("The door is locked");
-                    //UI.setPanel("Door", true);
-                    Debug.DrawRay(other.transform.position, other.transform.forward * 5f, Color.blue); //Draws a SCALED ray
+                
+                // SUB-CASE: When the player is behind a locked door
+                } else {
+                    //Debug.DrawRay(other.transform.position, other.transform.forward * 5f, Color.blue); //Draws a SCALED ray
 
-                    Ray ray = new Ray(other.transform.position, other.transform.forward);
-                    Vector3 endpoint = ray.origin + ray.direction * .8f;
+                    //This ray will be used to contruct another ray
+                    Ray ray0 = new Ray(other.transform.position, other.transform.forward);
+                    Vector3 endpoint = ray0.origin + ray0.direction * .8f;
                     //Debug.Log("The endpoint is: " + endpoint);
-                    Debug.DrawRay(endpoint, -other.transform.forward * 10f, Color.red);
+                    //Debug.DrawRay(endpoint, -other.transform.forward * 10f, Color.red);
 
+                    //Only perform the raycast when the endpoint is in front of the door. Think of the endpoint as a player
                     if (!isBehindDoor(endpoint)){
-                        Ray ray2 = new Ray(endpoint, -other.transform.forward);
-                        RaycastHit hit;
-                        float distance = 5;
-                        if (Physics.Raycast(ray2, out hit, distance))
-                        {
-                            Debug.Log("TAG: " + hit.collider.gameObject.tag);
-                            //Debug.Log("NAME: " + hit.collider.gameObject.name);
-                            if (hit.collider.gameObject.CompareTag(nametag))
-                            {
-                                UI.setDoorStateText("The door is locked");
-                                UI.setPanel("Door", true);
-                            }
-                            else
-                            {
-                                UI.setPanel("Door", false);
-                            }
-
+                        Ray ray = new Ray(endpoint, -other.transform.forward);
+                        if (isFacingDoor(ray)){
+                            UI.setDoorStateText("The door is locked");
+                            UI.setPanel("Door", true);
+                        } else {
+                            Debug.Log("The raycast isn't hitting " + nametag);
+                            UI.setPanel("Door", false);
                         }
-                    } else{
+                    } else {
                         UI.setPanel("Door", false);
                     }
-
-
                 }
             }
         }
@@ -147,12 +114,13 @@ public class MyDoor : MonoBehaviour {
         }
     }
 
-    private bool isFacingDoor(Transform player){
-        float distance = 5;
+    private bool isFacingDoor(Ray ray){
         RaycastHit hit;
+        float distance = 5;
 
-        if (Physics.Raycast(player.position, player.forward, out hit, distance) && hit.collider.gameObject.CompareTag(nametag)){
-            Debug.DrawLine(player.position, hit.transform.position);
+        if (Physics.Raycast(ray, out hit, distance) && hit.collider.gameObject.CompareTag(nametag)){
+            //Debug.Log("TAG: " + hit.collider.gameObject.tag);
+            //Debug.Log("NAME: " + hit.collider.gameObject.name);
             return true;
         } else {
             return false;
