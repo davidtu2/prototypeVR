d39f4bd5527518bf26863cc4894a0d1fa4c0c72d
David Tu <david.tu2@csu.fullerton.edu>
2019-07-19 17:40:55 -0700

    Implemented Sun and Moon

---
+++ VMG_Unity_Plugins V. 1.2.0/Assets/DayNightCycle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour {
+    public float speed;
+
+    void Start(){
+        speed = 20f; // Controls the speed of the sun and moon
+    }
+
+    void Update () {
+        transform.Rotate(speed * Time.deltaTime, 0, 0); //Can only rotate on the x or z axis
+	}
+}
